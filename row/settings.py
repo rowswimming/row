3375cc312ded36ace6a6aeb46da12b738abaea55
Hildeberto Mendonca <me@hildeberto.com>
2025-09-20 19:53:52 -0400

    Activated the modules

--- row/settings.py
+++ row/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k!)efxa(02w!291qdx@+$ucsu9y2x!k12ib(qv1t6ls1@qw4cr'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-k!)efxa(02w!291qdx@+$ucsu9y2x!k12ib(qv1t6ls1@qw4cr')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.rowswimming.ca']
 
@@ -36,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'club',
+    'event',
+    'volunteering',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Atikokan'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
