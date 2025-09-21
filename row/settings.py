02b0bed3819b8c380b3a533835ee9e4ddb9680e1
Hildeberto Mendonca <me@hildeberto.com>
2025-09-20 20:06:55 -0400

    Change required by the server

--- row/settings.py
+++ row/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR / 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
